0769187dc79e14a5ceca250933b6bbf09f733622
xrayd <72351211+xrayd@users.noreply.github.com>
2021-12-22 15:40:17 -0500

    advanced security fix

--- src/components/iconsource.js
+++ src/components/iconsource.js
@@ -2,7 +2,7 @@ import './style/footer.css'
 
 const Iconsource = () => (
     <div className="iconsource">
-        <a className="iconlink" href="https://icons8.com/icon/32675/school-director-male-skin-type-4">School Director Male Skin Type 4</a> icon by <a className="iconlink" target="_blank" href="https://icons8.com">Icons8</a>
+        <a className="iconlink" href="https://icons8.com/icon/32675/school-director-male-skin-type-4">School Director Male Skin Type 4</a> icon by <a className="iconlink" href="https://icons8.com">Icons8</a>
     </div>
 )
 
