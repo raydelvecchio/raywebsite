c4d6a96b71e1eae31499738d23fb2ad6fd078a77
xrayd <72351211+xrayd@users.noreply.github.com>
2021-12-21 20:05:16 -0500

    advanced footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,14 @@
 import './footer.css'
-import email from './resources/email.png'
-import github from './resources/github.png'
-import linkedin from './resources/linkedin.png'
+import Tripleicons from './tripleicons'
 
+/** sticky footer */
 const Footer = () => (
-    <footer className="footer">
-        <p className="copyrightname">&copy; Raymond Del Vecchio 2022
-            <a href="https://www.linkedin.com/in/raymond-del-vecchio/">
-                <img className="icon" src={linkedin}  alt="linkedin"/>
-            </a>
-            <a href="https://github.com/raydelvecchio">
-                <img className="icon" src={github}  alt="github"/>
-            </a>
-            <a href="mailto:raymond_del_vecchio@brown.edu">
-                <img className="icon" src={email}  alt="email"/>
-            </a>
-        </p>
-    </footer>
+    <div>
+        <footer className="footer">
+            <p className="copyrightname">&copy; Raymond Del Vecchio 2022</p>
+        </footer>
+        <Tripleicons/>
+    </div>
 );
 
 export default Footer;
---
+++ src/components/tripleicons.js
@@ -0,0 +1,20 @@
+import './footer.css'
+import email from './resources/email.png'
+import github from './resources/github.png'
+import linkedin from './resources/linkedin.png'
+
+const Tripleicons = () => (
+    <div className="icons">
+        <a href="https://www.linkedin.com/in/raymond-del-vecchio/" target="_blank">
+            <img className="icon" src={linkedin}  alt="linkedin"/>
+        </a>
+        <a href="https://github.com/raydelvecchio" target="_blank">
+            <img className="icon" src={github}  alt="github"/>
+        </a>
+        <a href="mailto:raymond_del_vecchio@brown.edu">
+            <img className="icon" src={email}  alt="email"/>
+        </a>
+    </div>
+)
+
+export default Tripleicons
